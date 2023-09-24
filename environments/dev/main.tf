2dec64e5c5789007bf93fa02da0b1c580137961b
mohdshine123 <139389575+mohdshine123@users.noreply.github.com>
2023-09-24 19:24:45 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -10,15 +10,15 @@ module "vpc" {
 }
 
 # Instance and ALB Module
-module "webapp" {
-  source = "../../modules/webapp"
+#module "webapp" {
+  #source = "../../modules/webapp"
 
-  vpc_id             = module.vpc.vpc_id
-  environment        = var.environment
-  public_subnet_ids  = module.vpc.public_subnet_ids
-  private_subnet_ids = module.vpc.private_subnet_ids
-  instance_type      = var.instance_type
-  key_name           = var.key_name
+  #vpc_id             = module.vpc.vpc_id
+  #environment        = var.environment
+  #public_subnet_ids  = module.vpc.public_subnet_ids
+  #private_subnet_ids = module.vpc.private_subnet_ids
+  #instance_type      = var.instance_type
+  3key_name           = var.key_name
 
-}
+#}
 
